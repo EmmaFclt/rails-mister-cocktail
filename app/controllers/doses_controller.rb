3e122a2a92377cf6f4ff8736b7d26a1b91a753d2
Emma Foucault <foucault.emma@gmail.com>
2020-01-08 11:36:44 +0100

    paramétrage des doses

--- app/controllers/doses_controller.rb
+++ app/controllers/doses_controller.rb
@@ -4,7 +4,10 @@ class DosesController < ApplicationController
   end
 
   def create
+    raise
+    @cocktail = Cocktail.find(params[:cocktail_id])
     @dose = Dose.new(doses_params)
+    @dose.cocktail = @cocktail
     @dose.save
   end
 
@@ -12,10 +15,10 @@ class DosesController < ApplicationController
     @dose = Dose.find(params[:id])
     @dose.destroy
   end
-end
 
-private
+  private
 
-def doses_params
-  params.require(:dose).permit(:description, :cocktail_id, :ingredient_id)
+  def doses_params
+    params.require(:dose).permit(:description, :ingredient_id)
+  end
 end
